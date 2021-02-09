55d514d3857ba457dc4111be0d0a80d6f67fab39
nickkutsevolov <idontwanttousemyactualemail@whatever.com>
2021-02-10 01:45:26 +0300

    Asveta landing created

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+
+function Header () {
+
+  return (
+    <header>
+      <div className="header-container">
+        <div className="logo">
+          <img src={logo} alt="asveta" />
+        </div>
+        <div className="nav-bar">
+          <div className="header-block"><a className="nav-bar-link">О школе</a></div>
+          <div className="header-block"><a className="nav-bar-link">Преподаватели</a></div>
+          <div className="header-block"><a className="nav-bar-link">Стоимость</a></div>
+          <div className="separator"></div>
+          <div className="header-block"><a className="nav-bar-link">Хочу преподавать</a></div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import hero_img from '../assets/hero_img.png';
+
+function Hero () {
+
+  return (
+    <div className="hero-wrapper">
+      <div className="hero">
+        <div className="hero-img-wrapper">
+          <img src={hero_img} alt="Asveta" className="img-max-width"/>
+        </div>
+        <h1>Онлайн уроки для закрепления школьной программы</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
