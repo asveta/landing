cff44fa80f973779b08d12b71691a1ff77fd37e3
nickkutsevolov <idontwanttousemyactualemail@whatever.com>
2021-02-11 14:46:04 +0300

    output item fix

--- src/components/FormOutput.js
+++ src/components/FormOutput.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 function FormOutput({ request }) {
-  console.log("values: " + [...request]);
   return (
     <div className="form-output-wrapper mv-24">
       <h3 className="mb-16">Вас интересует:</h3>
       <div className="form-output">
-        {Object.values(request).map((item) => {
-          <div className="form-item">{item}</div>;
-        })}
+        {Object.values(request).map( item => <div className="form-item">{item}</div> )}
       </div>
     </div>
   );
