294f2316d3e86be5a0445535a713bd0109c72683
Kiryl Kavalenka <kiryl@valvespace.com>
2021-02-12 13:38:12 +0300

    Add event to telegram link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import React from "react";
 import logo from "../assets/logo_white.png";
 import telegram from "../assets/telegram_logo.png";
+import firebase from "firebase";
 
 function Footer() {
   return (
     <div className="section-wrapper bg-black">
       <div className="footer">
         <img src={logo} alt="asveta" className="footer-logo" />
-        <a href="https://t.me/asvetaorg" className="telegram-link">
+        <a
+          href="https://t.me/asvetaorg"
+          className="telegram-link"
+          target="_blank"
+          onClick={() => {
+            firebase.analytics().logEvent("visit_telegram");
+          }}
+        >
           <img src={telegram} alt="telegram" />
           <span className="telegram-text">Наш Telegram канал</span>
         </a>
